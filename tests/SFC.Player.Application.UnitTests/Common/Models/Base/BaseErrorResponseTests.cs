61e6cb5e60ff13a3f98cf3b6805a570bffd6999b
Andrii Kryvoruk <AndriiK@neogames.com>
2025-05-23 10:19:07 +0300

    Add new infrastructure 2

--- tests/SFC.Player.Application.UnitTests/Common/Models/Base/BaseErrorResponseTests.cs
+++ tests/SFC.Player.Application.UnitTests/Common/Models/Base/BaseErrorResponseTests.cs
@@ -1,42 +1,42 @@
-﻿using System.Text.Json;
+﻿//using System.Text.Json;
 
-using Newtonsoft.Json.Linq;
+//using Newtonsoft.Json.Linq;
 
-namespace SFC.Player.Application.UnitTests.Common.Models.Base;
-public class BaseErrorResponseTests
-{
-    //[Fact]
-    //[Trait("Model", "BaseErrorResponse")]
-    //public void Model_BaseErrorResponse_ShouldHaveDefinedMessageAndSuccessAndErrorsValues()
-    //{
-    //    // Arrange
-    //    string message = "Test message";
-    //    Dictionary<string, IEnumerable<string>> errors = new() { { "Key", new List<string> { "Test error message." } } };
-    //    BaseErrorResponse response = new(message, errors);
+//namespace SFC.Player.Application.UnitTests.Common.Models.Base;
+//public class BaseErrorResponseTests
+//{
+//    //[Fact]
+//    //[Trait("Model", "BaseErrorResponse")]
+//    //public void Model_BaseErrorResponse_ShouldHaveDefinedMessageAndSuccessAndErrorsValues()
+//    //{
+//    //    // Arrange
+//    //    string message = "Test message";
+//    //    Dictionary<string, IEnumerable<string>> errors = new() { { "Key", new List<string> { "Test error message." } } };
+//    //    BaseErrorResponse response = new(message, errors);
 
-    //    // Assert
-    //    Assert.Equal(message, response.Message);
-    //    Assert.False(response.Success);
-    //    Assert.Equal(errors, response.Errors);
-    //}
+//    //    // Assert
+//    //    Assert.Equal(message, response.Message);
+//    //    Assert.False(response.Success);
+//    //    Assert.Equal(errors, response.Errors);
+//    //}
 
-    //[Fact]
-    //[Trait("Model", "BaseErrorResponse")]
-    //public void Model_BaseResponse_ShouldHaveCorrectPropertiesOrder()
-    //{
-    //    // Arrange
-    //    BaseErrorResponse response = new();
+//    //[Fact]
+//    //[Trait("Model", "BaseErrorResponse")]
+//    //public void Model_BaseResponse_ShouldHaveCorrectPropertiesOrder()
+//    //{
+//    //    // Arrange
+//    //    BaseErrorResponse response = new();
 
-    //    // Act
-    //    string serializedBaseResponse = JsonSerializer.Serialize(response);
-    //    JObject jsonObj = JObject.Parse(serializedBaseResponse);
-    //    JProperty[] properties = jsonObj.Properties().ToArray();
+//    //    // Act
+//    //    string serializedBaseResponse = JsonSerializer.Serialize(response);
+//    //    JObject jsonObj = JObject.Parse(serializedBaseResponse);
+//    //    JProperty[] properties = jsonObj.Properties().ToArray();
 
-    //    // Assert
+//    //    // Assert
 
-    //    Assert.Equal(3, properties.Count());
-    //    Assert.Equal(nameof(BaseErrorResponse.Success), properties[0].Name);
-    //    Assert.Equal(nameof(BaseErrorResponse.Message), properties[1].Name);
-    //    Assert.Equal(nameof(BaseErrorResponse.Errors), properties[2].Name);
-    //}
-}
+//    //    Assert.Equal(3, properties.Count());
+//    //    Assert.Equal(nameof(BaseErrorResponse.Success), properties[0].Name);
+//    //    Assert.Equal(nameof(BaseErrorResponse.Message), properties[1].Name);
+//    //    Assert.Equal(nameof(BaseErrorResponse.Errors), properties[2].Name);
+//    //}
+//}
--- tests/SFC.Player.Infrastructure.UnitTests/Services/Hosted/DataInitializationHostedServiceTests.cs
+++ tests/SFC.Player.Infrastructure.UnitTests/Services/Hosted/DataInitializationHostedServiceTests.cs
@@ -1,37 +1,37 @@
-﻿using MassTransit;
+﻿//using MassTransit;
 
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
 
-using Moq;
+//using Moq;
 
-using SFC.Player.Infrastructure.Services.Hosted;
+//using SFC.Player.Infrastructure.Services.Hosted;
 
-namespace SFC.Player.Infrastructure.UnitTests.Services.Hosted;
-public class DataInitializationHostedServiceTests
-{
-    //private readonly Mock<ILogger<DataInitializationHostedService>> _loggerMock = new();
+//namespace SFC.Player.Infrastructure.UnitTests.Services.Hosted;
+//public class DataInitializationHostedServiceTests
+//{
+//    //private readonly Mock<ILogger<DataInitializationHostedService>> _loggerMock = new();
 
-    //[Fact]
-    //[Trait("Service", "DataInitializationHosted")]
-    //public async Task Service_Hosted_DataInitialization_ShouldPublishDataRequireEvent()
-    //{
-    //    // Arrange
-    //    IServiceCollection services = new ServiceCollection();
-    //    Mock<IPublishEndpoint> publishMock = new();
-    //    DataRequireMessage @event = null!;
-    //    publishMock.Setup(p => p.Publish(It.IsAny<DataRequireMessage>(), It.IsAny<CancellationToken>()))
-    //        .Callback<DataRequireMessage, CancellationToken>((assertEvent, _) => @event = assertEvent);
-    //    services.AddSingleton(publishMock.Object);
+//    //[Fact]
+//    //[Trait("Service", "DataInitializationHosted")]
+//    //public async Task Service_Hosted_DataInitialization_ShouldPublishDataRequireEvent()
+//    //{
+//    //    // Arrange
+//    //    IServiceCollection services = new ServiceCollection();
+//    //    Mock<IPublishEndpoint> publishMock = new();
+//    //    DataRequireMessage @event = null!;
+//    //    publishMock.Setup(p => p.Publish(It.IsAny<DataRequireMessage>(), It.IsAny<CancellationToken>()))
+//    //        .Callback<DataRequireMessage, CancellationToken>((assertEvent, _) => @event = assertEvent);
+//    //    services.AddSingleton(publishMock.Object);
 
-    //    IHostedService service = new DataInitializationHostedService(_loggerMock.Object, services.BuildServiceProvider());
+//    //    IHostedService service = new DataInitializationHostedService(_loggerMock.Object, services.BuildServiceProvider());
 
-    //    // Act
-    //    await service.StartAsync(new CancellationToken());
+//    //    // Act
+//    //    await service.StartAsync(new CancellationToken());
 
-    //    // Assert
-    //    publishMock.Verify(mock => mock.Publish(It.IsAny<DataRequireMessage>(), It.IsAny<CancellationToken>()), Times.Once());
-    //    Assert.Equal(DataInitiator.Player, @event!.Initiator);
-    //}
-}
+//    //    // Assert
+//    //    publishMock.Verify(mock => mock.Publish(It.IsAny<DataRequireMessage>(), It.IsAny<CancellationToken>()), Times.Once());
+//    //    Assert.Equal(DataInitiator.Player, @event!.Initiator);
+//    //}
+//}
